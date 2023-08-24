b82b18035dcbffab1a896bee1ddea4a990b138fe
toxazol <antonmelni20@gmail.com>
2023-08-25 01:43:18 +0900

    make Caret dspTime based

--- Assets/Scripts/Caret.cs
+++ Assets/Scripts/Caret.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public struct Note 
+public struct Instrument 
 {
-    public GameObject note;
+    public AudioClip sound;
     public KeyCode keyCode;
+}
+
+[System.Serializable]
+public class Note 
+{
+    public AudioClip sound;
+    public double time;
+    public Note(AudioClip sound, double time)
+    {
+        this.sound = sound;
+        this.time = time;
+    }
 }
 
 [System.Serializable]
@@ -25,24 +37,48 @@ public class Caret : MonoBehaviour
     [SerializeField] private int barCount = 4;
     [SerializeField] private int ticksPerBar = 4;
 
-    [SerializeField] private Note[] notes;    
-    // [SerializeField] private Tick tick;
+    [SerializeField] private Instrument[] instruments;    
+    [SerializeField] private List<Note> melody = new();    
+    [SerializeField] private Tick tick;
+    [SerializeField] private double tolerance = 0.125f;
     
 
     private bool isStarted = false;
     private Vector3 startingPosition;
-    // private Dictionary<double, AudioSource> noteTimeStamps = new();
-    private float trackTime;
+    private double trackTime; // track length in seconds
+    private double now; // current time coordinate
+    private int loops = 0; // loops counter
     private float caretSpeed;
+    private double startPlayTime; 
+    private int closestNoteInd = -1;
 
-    // Start is called before the first frame update
+    private AudioSource audioSource;
+    
+
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
+
         startingPosition = transform.position;
+        trackTime = barCount * ticksPerBar * 60f / BPM;
+        caretSpeed = (float)(pixelsInTrack / trackTime);
+
+        InitTicker();
     }
 
-    // Update is called once per frame
-    void Update()
+    void InitTicker()
+    {
+        double startTime = 0;
+        for(int i = 0; i < barCount * ticksPerBar; i++)
+        {
+            double tickTime = startTime + i * 60f / BPM;
+            AudioClip nextTick = i % 4 == 0 ? tick.tac : tick.tic;
+            Note note = new(nextTick, tickTime);
+            melody.Add(note);
+        }
+    }
+
+    void Update() // TODO: -> FixedUpdate?
     {
         if(!isStarted)
         {
@@ -51,17 +87,44 @@ public class Caret : MonoBehaviour
 
         CheckRestart();
 
-        trackTime = barCount * ticksPerBar * 60f / BPM;
-        caretSpeed = pixelsInTrack / trackTime;
+        CheckMelody();
+
         transform.Translate(caretSpeed * Time.deltaTime, 0, 0);
 
         CheckInput();
     }
 
-    public void UnPause()
+    void CheckMelody() 
+    {
+        now = AudioSettings.dspTime - startPlayTime - loops * trackTime;
+        if(closestNoteInd == -1)
+        {
+            //find closest note
+            closestNoteInd = melody.FindIndex(0, melody.Count-1, note => note.time > now);
+        }
+        if(closestNoteInd < 0 || closestNoteInd >= melody.Count)
+        {
+            Debug.Log("closestNoteInd="+ closestNoteInd + " is out of bounds");
+            return;
+        }
+
+        Note closestNote = melody[closestNoteInd];
+        if(now + tolerance > closestNote.time)
+        {
+            double realNoteTime = closestNote.time + loops * trackTime;
+            audioSource.clip = closestNote.sound; 
+            audioSource.PlayScheduled(realNoteTime);
+            closestNoteInd++;
+        }
+    
+    }
+
+    public void StartPlay()
     {
         isStarted = true;
-        StartCoroutine(PlaySoundAtInterval(GetComponent<AudioSource>(), 60f / BPM));
+        startPlayTime = AudioSettings.dspTime;
+        // audioSource.PlayOneShot(tick.tac); // TODO: only if ticker enabled
+        // StartCoroutine(PlaySoundAtInterval(GetComponent<AudioClip>(), 60f / BPM));
         // StartCoroutine(StartWith4Ticks());
     }
 
@@ -70,12 +133,14 @@ public class Caret : MonoBehaviour
         if(Mathf.Abs(transform.position.x - startingPosition.x) > pixelsInTrack)
         {
             transform.position = startingPosition;
+            closestNoteInd = 0;
+            loops++;
         }
     }
     
     void CheckInput() 
     {
-        foreach (var note in notes)
+        foreach (var note in instruments)
         {
             if(Input.GetKeyDown(note.keyCode))
             {
@@ -84,31 +149,31 @@ public class Caret : MonoBehaviour
         }
     }
 
-    void AddNote(Note note)
+    void AddNote(Instrument note)
     {
-        var noteInstance = Instantiate(note.note, transform.parent.transform, false);
-        noteInstance.transform.Translate(transform.position.x - noteInstance.transform.position.x, 0, 0);
+        // var noteInstance = Instantiate(note.sound, transform.parent.transform, false);
+        // noteInstance.transform.Translate(transform.position.x - noteInstance.transform.position.x, 0, 0);
 
-        StartCoroutine(PlaySoundAtInterval(noteInstance.GetComponent<AudioSource>(), trackTime));
+        // StartCoroutine(PlaySoundAtInterval(noteInstance.GetComponent<AudioClip>(), trackTime));
     }
 
-    private IEnumerator PlaySoundAtInterval(AudioSource sound, float interval)
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(interval);
-            sound.Play();
-        }
-    }
+    // private IEnumerator PlaySoundAtInterval(AudioClip sound, double interval)
+    // {
+    //     while (true)
+    //     {
+    //         yield return new WaitForSeconds(interval);
+    //         sound.Play();
+    //     }
+    // }
 
-    private IEnumerator StartWith4Ticks()
-    {
-        for (int i=0; i<ticksPerBar; i++)
-        {
-            yield return new WaitForSeconds(60f / BPM);
-            GetComponent<AudioSource>().Play();
-        }
-        isStarted = true;
-    }
+    // private IEnumerator StartWith4Ticks()
+    // {
+    //     for (int i=0; i<ticksPerBar; i++)
+    //     {
+    //         yield return new WaitForSeconds(60f / BPM);
+    //         GetComponent<AudioClip>().Play();
+    //     }
+    //     isStarted = true;
+    // }
 
 }
