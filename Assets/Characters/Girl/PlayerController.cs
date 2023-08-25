00b6f16c1eae205dca1983d63c883fc0e78c302a
mhryb <mykyta_hryb@epam.com>
2023-08-25 19:44:09 +0900

    add Invulnerability timer

--- Assets/Characters/Girl/PlayerController.cs
+++ Assets/Characters/Girl/PlayerController.cs
@@ -12,6 +12,7 @@ public class PlayerController : MonoBehaviour, IDamageable
     [SerializeField] private float collisionOffset = 0.05f;
     [SerializeField] private Image hpUI;
     [SerializeField] private AttackZone attackZone;
+    [SerializeField] private float invulnerabilityTime = 1f;
 
     private Vector2 moveInput;
     private Rigidbody2D rb;
@@ -20,7 +21,7 @@ public class PlayerController : MonoBehaviour, IDamageable
     private SpriteRenderer spriteRenderer;
     private Blinking blinking;
     private List<RaycastHit2D> castCollisions = new();
-
+    private int isInvulnerable = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -106,6 +107,9 @@ public class PlayerController : MonoBehaviour, IDamageable
 
     public void OnHit(int damage)
     {
+        if (isInvulnerable > 0) return;
+
+        StartInvulnerability();
         health.Hit(damage);
         hpUI.fillAmount = health.GetHealthPercentage();
         blinking.Blink();
@@ -138,6 +142,17 @@ public class PlayerController : MonoBehaviour, IDamageable
         attackZone.transform.localScale = new Vector3(isTurnedLeft() ? -1 : 1, 1, 1);
         animator.SetTrigger("Attack");
         Invoke("EndAttack", 0.3f);
+    }
+
+    private void StartInvulnerability()
+    {
+        isInvulnerable++;
+        Invoke("CancelInvulnerability", invulnerabilityTime);
+    }
+    
+    private void CancelInvulnerability()
+    {
+        isInvulnerable--;
     }
 
     private void EndAttack()
